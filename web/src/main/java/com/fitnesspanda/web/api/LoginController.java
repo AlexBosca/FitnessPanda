8f5877db14002f470c1594d5f23df97de035a649
AlexBosca <alex_241998@yahoo.com>
2021-04-28 16:00:46 +0300

    Updated For Signing Up Process

--- web/src/main/java/com/fitnesspanda/web/api/LoginController.java
+++ web/src/main/java/com/fitnesspanda/web/api/LoginController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -16,15 +18,15 @@ public class LoginController {
     @Autowired
     private UserRepository userRepo;
 
+    //May be deleted
+    @GetMapping("")
+    public String viewDefaultPage() {
+        return "login";
+    }
+
     @GetMapping("/login")
     public String viewLoginPage() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
             return "login";
-        }
-
-        return "redirect:/";
     }
 
     @GetMapping("/sign_up")
@@ -32,5 +34,21 @@ public class LoginController {
         model.addAttribute("user", new User());
 
         return "sign_up";
+    }
+
+    @GetMapping("/home")
+    public String viewHomePage() {
+        return "home";
+    }
+
+    @PostMapping("/process_sign_up")
+    public String processSignUp(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "sign_up_success";
     }
 }
